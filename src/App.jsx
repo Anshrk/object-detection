209be0f6dbb3936bbca491553263613b42442d2d
Anshrk <anshrk.13@gmail.com>
2021-04-29 17:47:21 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,25 @@
 // Import dependencies
 import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
-// 1. TODO - Import required model here
-// e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 
 import Webcam from "react-webcam";
 import "./App.css";
-// 2. TODO - Import drawing utility here
-// e.g. import { drawRect } from "./utilities";
+import {drawRect} from "./draw";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  // Main function
   const runCoco = async () => {
-    // 3. TODO - Load network 
-    // e.g. const net = await cocossd.load();
-    
-    //  Loop and detect hands
+    // cocoss.load() reads the web cam
+    const net = await cocossd.load();
     setInterval(() => {
       detect(net);
     }, 10);
   };
 
   const detect = async (net) => {
-    // Check data is available
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
@@ -45,14 +38,15 @@ function App() {
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      // 4. TODO - Make Detections
-      // e.g. const obj = await net.detect(video);
-
+      // Make Detections
+      const obj = await net.detect(video);
+      console.log(obj);
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
       // drawSomething(obj, ctx)  
+      drawRect(obj, ctx);
     }
   };
 
---
+++ src/draw.js
@@ -0,0 +1,19 @@
+export const drawRect = ( detections, ctx ) => {
+    detections.forEach(prediction => {
+        const [x, y, width, height] = prediction['bbox'];
+        const text = prediction['class'];
+
+        // styling the lines and font
+        const color = '#' + Math.floor(Math.random()*16777215).toString(16);
+        ctx.strokeStyle = color;
+        ctx.font = '30px Arial';
+        ctx.fillStyle = color;
+
+        // drawing the line
+        ctx.beginPath()
+        ctx.fillText(text, x, y)
+        ctx.rect(x, y, width, height)
+        ctx.stroke()
+        
+    });
+};
